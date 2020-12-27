31ca94afb16989ac16fa0dc4b5b1a246f0df829c
aniledev <elinamcgill@gmail.com>
2020-12-27 13:19:00 -0500

    feat: create sample state object for testing within App component

--- src/App/App.js
+++ src/App/App.js
@@ -1,16 +1,24 @@
+import React from "react";
 import "./App.css";
 import AddItemForm from "../Form/AddItemForm";
 import ShoppingList from "../ShoppingList/ShoppingList";
 import ShoppingItems from "../ShoppingItems/ShoppingItems";
 
-function App() {
-  return (
-    <div className="App">
-      <AddItemForm className="Form" />
-      <ShoppingList className="ShoppingList" />
-      <ShoppingItems className="ShoppingItems" />
-    </div>
-  );
-}
+// this component will contain teh app's title, the add item form, and the shopping list
+// it also contains sample state
 
-export default App;
+import react from "react";
+
+export default class App extends React.Component {
+  // initialize state for App component because its the lowest common ancestor of the components that update and read state
+  state = {
+    ShoppingItems: [
+      { name: "apples", checked: false },
+      { name: "oranges", checked: true },
+      { name: "bread", checked: false },
+    ],
+  };
+  render() {
+    return <div></div>;
+  }
+}
