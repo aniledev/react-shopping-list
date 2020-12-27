a77017733d10f86d4115032fc1c3a6a27e1ef1ee
aniledev <elinamcgill@gmail.com>
2020-12-27 15:54:03 -0500

    test: write snapshot testing for AddItemForm component

--- src/Form/AddItemForm.test.js
+++ src/Form/AddItemForm.test.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import AddItemForm from "./AddItemForm";
+import { shallow } from "enzyme";
+import toJson from "enzyme-to-json";
 
 describe("AddItemForm Component", () => {
   // start with a smoke test inside a describe block; does the component render properly without errors
@@ -8,5 +10,11 @@ describe("AddItemForm Component", () => {
     const div = document.createElement("div");
     ReactDOM.render(<AddItemForm />, div);
     ReactDOM.unmountComponentAtNode(div);
+  });
+
+  // use enzyme to write a snapshot test: does the component render the form completely
+  it("should render the complete form", () => {
+    const wrapper = shallow(<AddItemForm />);
+    expect(toJson(wrapper)).toMatchSnapshot();
   });
 });
--- src/ShoppingList/ShoppingList.test.js
+++ src/ShoppingList/ShoppingList.test.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { shallow } from "enzyme";
+import toJson from "enzyme-to-json";
 import ShoppingList from "./ShoppingList";
 
 describe("ShoppingList Component", () => {
@@ -8,5 +10,22 @@ describe("ShoppingList Component", () => {
     const div = document.createElement("div");
     ReactDOM.render(<ShoppingList />, div);
     ReactDOM.unmountComponentAtNode(div);
+  });
+
+  // if there are no items in the array, then nothing should render
+  it("should render empty given no items", () => {
+    const wrapper = shallow(<ShoppingList />);
+    expect(toJson(wrapper)).toMatchSnapshot();
+  });
+
+  // it should render multiple items when supplied with a proper array of items
+  it("should render ShoppingItems when supplied with an items prop", () => {
+    const testItems = [
+      { name: "test item 1", checked: false },
+      { name: "another test item", checked: true },
+      { name: "test item number 3", checked: false },
+    ];
+    const wrapper = shallow(<ShoppingList items={testItems} />);
+    expect(toJson(wrapper)).toMatchSnapshot();
   });
 });
