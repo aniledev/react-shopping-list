36568bc3c99fad7daab1c44c95723f77e8db2efd
aniledev <elinamcgill@gmail.com>
2020-12-27 12:48:20 -0500

    feat: create Form, ShoppingItems, and ShoppingList components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import "./App.css";
+import Form from "./Form";
+import ShoppingList from "./ShoppingList";
+import ShoppingItems from "./ShoppingItems";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Form className="Form" />
+      <ShoppingList className="ShoppingList" />
+      <ShoppingItems className="ShoppingItems" />
+    </div>
+  );
 }
 
 export default App;
