bef1c47e713ade33f3199ce0e34eccb732d6aec8
aniledev <elinamcgill@gmail.com>
2020-12-27 13:59:12 -0500

    style: added in missing curly braces

--- src/ShoppingItems/ShoppingItems.js
+++ src/ShoppingItems/ShoppingItems.js
@@ -1,15 +1,19 @@
 import React from "react";
 
-// a list containing all of the items
-
-export default function ShoppingItems (props) {
-  ShoppingItems.defaultProps = {
-    item: {}
+export default function ShoppingItem(props) {
+  ShoppingItem.defaultProps = {
+    item: {},
   };
-  
+
   return (
     <li>
-      <h2 style {{ textDecoration: props.item.checked ? "line-through" : null, }}>{props.item.name}</h2>
+      <h2
+        style={{
+          textDecoration: props.item.checked ? "line-through" : null,
+        }}
+      >
+        {props.item.name}
+      </h2>
       <button type="button">check</button>
       <button type="button">delete</button>
     </li>
