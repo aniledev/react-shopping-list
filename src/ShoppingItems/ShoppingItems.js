16d4ecf07973fcf1701a801d2382f3fb89c1b4ba
aniledev <elinamcgill@gmail.com>
2020-12-27 13:57:46 -0500

    feat: update render method of ShoppingList component

--- src/ShoppingItems/ShoppingItems.js
+++ src/ShoppingItems/ShoppingItems.js
@@ -2,13 +2,16 @@ import React from "react";
 
 // a list containing all of the items
 
-export default class ShoppingItems extends React.Component {
-  render() {
-    // make sure that each list item contains a key
-    return (
-      <div>
-        <p>Content</p>
-      </div>
-    );
-  }
+export default function ShoppingItems (props) {
+  ShoppingItems.defaultProps = {
+    item: {}
+  };
+  
+  return (
+    <li>
+      <h2 style {{ textDecoration: props.item.checked ? "line-through" : null, }}>{props.item.name}</h2>
+      <button type="button">check</button>
+      <button type="button">delete</button>
+    </li>
+  );
 }
