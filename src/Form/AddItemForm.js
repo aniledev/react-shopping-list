2e1b1a8f56ff0c453a3961b33dfc863ffc3f2e64
aniledev <elinamcgill@gmail.com>
2020-12-27 18:36:15 -0500

    feat: create onSubmitForm method and pass in value from input to callback prop function

--- src/Form/AddItemForm.js
+++ src/Form/AddItemForm.js
@@ -3,20 +3,29 @@ import React from "react";
 // a form with an input and a submit button to add items to the list
 
 export default class AddItemForm extends React.Component {
+  // prevent default submission of the form
+  onSubmitForm = (e) => {
+    e.preventDefault();
+    // e.target['inputName'] references `<input name='inputName' />`
+    // pass in the value of the input form into the onAddItem callback props
+    this.props.onAddItem(e.target.add.value);
+  };
+
   // buttons are not wired up, not functional yet
   render() {
     return (
       <form className="form">
-        <label htmlFor="add-item" className="add-form" id="add-form">
+        <label htmlFor="add" className="add-form" id="add-form">
           Add Item
         </label>
         <input
           type="text"
-          className="add-item"
-          id="add-item"
+          className="add"
+          id="add"
           placeholder="carrots"
           aria-label="Shopping list item"
           min={1}
+          name="add"
         ></input>
         <button type="submit">Add Item</button>
       </form>
